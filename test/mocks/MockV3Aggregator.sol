14ee79de2d41bb65656205a046fcb5d9d74b29e2
mon-1 <walid.erraoui@outlook.com>
2023-07-05 22:08:50 +0100

    updated mocks

--- test/mocks/MockV3Aggregator.sol
+++ test/mocks/MockV3Aggregator.sol
@@ -1,31 +1,67 @@
-// // SPDX-License-Identifier: UNLICENSE
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.0;
 
-// contract MockV3Aggreagtor {
-//     uint256 public constant version = 0;
+contract MockV3Aggreagtor {
+    uint256 public constant version = 0;
 
-//     uint8 public decimals;
-//     int256 public latestAnswer;
-//     uint256 public latestTimestamp;
-//     uint256 public latestRound;
+    uint8 public decimals;
+    int256 public latestAnswer;
+    uint256 public latestTimestamp;
+    uint256 public latestRound;
 
-//     mapping(uint256 => int256) public getAnswer;
-//     mapping(uint256 => uint256) public getTimestamp;
-//     mapping(uint256 => uint256) public getStartedAt;
+    mapping(uint256 => int256) public getAnswer;
+    mapping(uint256 => uint256) public getTimestamp;
+    mapping(uint256 => uint256) public getStartedAt;
 
-//     constructor(uint8 _decimals, int256 _initialAnswer) {
-//         decimals = _decimals;
-//         updateAnswer(_initialAnswer);
-//     }
+    constructor(uint8 _decimals, int256 _initialAnswer) {
+        decimals = _decimals;
+        updateAnswer(_initialAnswer);
+    }
 
-//     function updateAsnwer(int256 _answer) public {
-//         latestAnswer = _answer;
-//         latestTimestamp = block.timestamp;
-//         latestRound++;
-//         getAnswer[latestRound] = _answer;
-//         getTimestamp[latestRound] = block.timestamp;
-//         getStartedAt[latestRound] = block.timestamp;
-//     }
+    function updateAnswer(int256 _answer) public {
+        latestAnswer = _answer;
+        latestTimestamp = block.timestamp;
+        latestRound++;
+        getAnswer[latestRound] = _answer;
+        getTimestamp[latestRound] = block.timestamp;
+        getStartedAt[latestRound] = block.timestamp;
+    }
 
-//     // function up
-// }
+    function updateRoundData(
+        uint80 _roundId,
+        int256 _answer,
+        uint256 _timestamp,
+        uint256 _startedAt
+    ) public {
+        latestRound = _roundId;
+        latestAnswer = _answer;
+        latestTimestamp = _timestamp;
+        getAnswer[latestRound] = _answer;
+        getTimestamp[latestRound] = _timestamp;
+        getStartedAt[latestRound] = _startedAt;
+    }
+
+    function getRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 srartedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return (
+            uint80(latestRound),
+            getAnswer[latestRound],
+            getStartedAt[latestRound],
+            getTimestamp[latestRound],
+            uint80(latestRound)
+        );
+    }
+
+    function decription() external pure returns (string memory) {
+        return "v0.6/tests/MockV3Aggreagtor.sol";
+    }
+}
